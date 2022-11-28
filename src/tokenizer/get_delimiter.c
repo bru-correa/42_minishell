f3adcb299aba78ab3ecb06fcfce037a1bc7c17ae
bru-correa <bruuh.cor@gmail.com>
2022-11-28 12:08:01 -0300

    fix: split environment variables into multiple tokens

--- src/tokenizer/get_delimiter.c
+++ src/tokenizer/get_delimiter.c
@@ -6,7 +6,7 @@
 /*   By: bcorrea- <bruuh.cor@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 13:49:10 by bcorrea-          #+#    #+#             */
-/*   Updated: 2022/11/04 16:47:38 by bcorrea-         ###   ########.fr       */
+/*   Updated: 2022/11/28 12:07:38 by bcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 static int	check_append(const char *cmdline, int current, int start);
 static int	check_heredoc(const char *cmdline, int current, int start);
 static int	check_pipe(int current, int start);
-static int	check_dolar_sign(int current, int start);
 
 int	get_delimiter_index(const char *cmdline, int current, int start)
 {
 	if (cmdline[current] == ' ')
 		return (current);
-	else if (cmdline[current] == '$')
-		return (check_dolar_sign(current, start));
+	// else if (cmdline[current] == '$')
+	// 	return (check_dolar_sign(current, start));
 	else if (cmdline[current] == '|')
 		return (check_pipe(current, start));
 	else if (cmdline[current] == '>')
@@ -30,15 +29,6 @@ int	get_delimiter_index(const char *cmdline, int current, int start)
 	else if (cmdline[current] == '<')
 		return (check_heredoc(cmdline, current, start));
 	return (-1);
-}
-
-// Only act as a delimiter when '$' is not at the start
-static int	check_dolar_sign(int current, int start)
-{
-	if (current != start)
-		return (current);
-	else
-		return (-1);
 }
 
 static int	check_pipe(int current, int start)
